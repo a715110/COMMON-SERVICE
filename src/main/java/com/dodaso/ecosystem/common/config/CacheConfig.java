3354503b3bfe6d1f3608ffce14937a18f8b9d9c6
Byoung Park <pbk97030@hotmail.com>
2026-09-20 16:16:24 -0400

    update

--- src/main/java/com/dodaso/ecosystem/common/config/CacheConfig.java
+++ src/main/java/com/dodaso/ecosystem/common/config/CacheConfig.java
@@ -5,18 +5,20 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-@Configuration
+@Configuration("commonCacheConfig")
 public class CacheConfig {
 
-  @Bean
-  public CaffeineCacheManager cacheManager() {
+  @Bean(name = "commonCacheManager")
+  @Primary
+  public CaffeineCacheManager commonCacheManager() {
     Caffeine<Object, Object> caffeine = Caffeine.newBuilder()
         .initialCapacity(100)
         .maximumSize(500)
         .expireAfterWrite(30, TimeUnit.MINUTES)
         .recordStats();
-    CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("ecws", "elcm");
+    CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("common");
     caffeineCacheManager.setCaffeine(caffeine);
     return caffeineCacheManager;
   }
